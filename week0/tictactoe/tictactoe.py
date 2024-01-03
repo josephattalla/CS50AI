47a98173d2d2646db23781ac9c33bed3167544a4
Joseph Attalla <jojoatt10@gmail.com>
2024-01-03 21:51:14 +0000

    Jan 3, 2024, 4:51 PM

--- week0/tictactoe/tictactoe.py
+++ week0/tictactoe/tictactoe.py
@@ -55,7 +55,7 @@ def actions(board):
     # create set of moves
     moves = set()
 
-    # 
+    # loop through rows and columns using enumerate to keep track of the index
     for i, row in enumerate(board):
         for j, col in enumerate(row):
             if col == EMPTY:
