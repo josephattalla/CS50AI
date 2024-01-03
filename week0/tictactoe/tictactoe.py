d5ab7d19aa564cd6151e0eb8f713f5124df63c9b
Joseph Attalla <jojoatt10@gmail.com>
2024-01-03 21:42:55 +0000

    Jan 3, 2024, 4:42 PM

--- week0/tictactoe/tictactoe.py
+++ week0/tictactoe/tictactoe.py
@@ -44,7 +44,7 @@ def actions(board):
     """
     moves = set()
 
-    for row in board:
+    for i, row in board:
         for col in row:
             if col == X: X_count+=1
             elif col == O: O_count+=1
