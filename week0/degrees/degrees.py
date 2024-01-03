0454bb4f18ed083dfee89d1d8df791904766a68e
Joseph Attalla <jojoatt10@gmail.com>
2024-01-03 19:37:18 +0000

    Jan 3, 2024, 2:37 PM

--- week0/degrees/degrees.py
+++ week0/degrees/degrees.py
@@ -114,7 +114,7 @@ def shortest_path(source, target):
 
         # If node is the goal, we have solution
         for pair in node:
-            if pair[1] in goal:
+            if pair[1] == goal:
 
                 return pair
 
