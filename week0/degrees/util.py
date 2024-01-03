f7928941028ed0195368c4d94ba9ee2ebcb8cc1f
Joseph Attalla <jojoatt10@gmail.com>
2024-01-03 18:49:51 +0000

    Jan 3, 2024, 1:49 PM

--- week0/degrees/util.py
+++ week0/degrees/util.py
@@ -36,7 +36,6 @@ class QueueFrontier(StackFrontier):
     def remove(self):
         if self.empty():
             raise Exception("empty frontier")
-        else:
             node = self.frontier[0]
             self.frontier = self.frontier[1:]
             return node
