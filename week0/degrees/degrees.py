81f547114a3c58664070ab3bfe8efd22756044bc
Joseph Attalla <jojoatt10@gmail.com>
2024-01-03 20:07:48 +0000

    Jan 3, 2024, 3:07 PM

--- week0/degrees/degrees.py
+++ week0/degrees/degrees.py
@@ -117,7 +117,7 @@ def shortest_path(source, target):
                 path = []
                 while node.parent is not None:
                     path.append((node.action, node.state))
-                    
+                    node 
                 path.reverse()
                 return path
 
