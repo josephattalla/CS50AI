d29f6b83122f31a033630428d9919e98e5ba9ae8
Joseph Attalla <jojoatt10@gmail.com>
2024-01-03 19:17:12 +0000

    Jan 3, 2024, 2:17 PM

--- week0/degrees/degrees.py
+++ week0/degrees/degrees.py
@@ -95,7 +95,7 @@ def shortest_path(source, target):
     # inital state
     state_0 = neighbors_for_person(source)
 
-    
+    # check if 
     # keep track of explored nodes (degrees)
     num_explored = 0
 
