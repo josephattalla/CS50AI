bd77f2423251d5664cd7c10651f200422286d371
Joseph Attalla <jojoatt10@gmail.com>
2024-01-03 18:49:26 +0000

    Jan 3, 2024, 1:49 PM

--- week0/degrees/util.py
+++ week0/degrees/util.py
@@ -18,6 +18,7 @@ class StackFrontier():
     def empty(self):
         return len(self.frontier) == 0
 
+
     # Made by CS50, modified by Joseph Attalla to remove uneccesary else statement
     # after the if: raise Exception
     def remove(self):
