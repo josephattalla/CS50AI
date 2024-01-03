2ebbc2336b10a21aba03c087d3188f47a9356a37
Joseph Attalla <jojoatt10@gmail.com>
2024-01-03 22:03:37 +0000

    Jan 3, 2024, 5:03 PM

--- week0/tictactoe/tictactoe.py
+++ week0/tictactoe/tictactoe.py
@@ -103,6 +103,7 @@ def winner(board):
         elif row == [O, O, O]:
             return O
         # check columns for winner
+        column = []
         for col in row:
             column.append()
     
