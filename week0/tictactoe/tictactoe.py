96bbde64a0bd20dc9a6dd5e693b83a7c380a9496
Joseph Attalla <jojoatt10@gmail.com>
2024-01-03 22:17:26 +0000

    Jan 3, 2024, 5:17 PM

--- week0/tictactoe/tictactoe.py
+++ week0/tictactoe/tictactoe.py
@@ -130,7 +130,7 @@ def terminal(board):
         return True
     
     # if there is no more empty spots, return True
-    return all(col != EMPTY)
+    return all(col != EMPTY for row in board for col in row)
 
 
 
