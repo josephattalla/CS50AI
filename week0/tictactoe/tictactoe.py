6d4e7cbe657b4f311c89009e25efca795174a099
Joseph Attalla <jojoatt10@gmail.com>
2024-01-03 21:52:23 +0000

    Jan 3, 2024, 4:52 PM

--- week0/tictactoe/tictactoe.py
+++ week0/tictactoe/tictactoe.py
@@ -71,7 +71,7 @@ def result(board, action):
     Returns the board that results from making move (i, j) on the board.
     """
 
-    # if the action is placing 
+    # if the action is placing a move on 
     if board[action] != EMPTY:
         raise Exception('Invalid Move!')
     new_board = board.deepcopy()
