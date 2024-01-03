a8a169865880a26f5a8d268eb6fdc1aac722c3d2
Joseph Attalla <jojoatt10@gmail.com>
2024-01-03 19:25:32 +0000

    Jan 3, 2024, 2:25 PM

--- week0/degrees/degrees.py
+++ week0/degrees/degrees.py
@@ -83,6 +83,7 @@ def main():
             movie = movies[path[i + 1][0]]["title"]
             print(f"{i + 1}: {person1} and {person2} starred in {movie}")
 
+
 def shortest_path(source, target):
     """
     Returns the shortest list of (movie_id, person_id) pairs
