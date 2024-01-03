b8344179a138467f1e376f413343111d9c3cff26
Joseph Attalla <jojoatt10@gmail.com>
2024-01-03 19:27:10 +0000

    Jan 3, 2024, 2:27 PM

--- week0/degrees/degrees.py
+++ week0/degrees/degrees.py
@@ -119,7 +119,9 @@ def shortest_path(source, target):
         node = frontier.remove()
 
         # If node is the goal, we have solution
-        
+        for pair in state_0:
+            if pair in goal:
+            return pair
 
 
 
