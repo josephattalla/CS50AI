73f97bf24d5ad0a571705238423692178d67731b
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 00:15:34 +0000

    Jan 3, 2024, 7:15 PM

--- week0/tictactoe/tictactoe.py
+++ week0/tictactoe/tictactoe.py
@@ -73,7 +73,7 @@ def result(board, action):
     """
 
     # if the action is placing a move on a spot that is not empty, raise Excpetion
-    if board[action[0]][action[1]] != EMPTY:
+    if board[action] != EMPTY:
         raise Exception('Invalid Move!')
     
     # make deepcopy of board
@@ -83,7 +83,7 @@ def result(board, action):
     user = player(board)
 
     # place user (player) in action (empty index on board)
-    new_board[action[0]][action[1]] = user
+    new_board[action] = user
 
     # return new_board
     return new_board
