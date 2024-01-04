da46685aee1a087d32491f91d0121c3186e536d9
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 00:13:17 +0000

    Jan 3, 2024, 7:13 PM

--- week0/tictactoe/tictactoe.py
+++ week0/tictactoe/tictactoe.py
@@ -216,7 +216,7 @@ def min_function(board):
         # create new board based on current move
         new_board = result(board, move)
         # get value of optimal move of other player using min_function
-        _, value = min_function(new_board)
+        _, value = max_function(new_board)
         
         # if value is greater than v, set v = value and best_action = move
         if value > v:
