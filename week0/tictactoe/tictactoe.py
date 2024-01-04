cb8fc2a8b4c3cd949158058d1f83dfacb0aa7681
Joseph Attalla <jojoatt10@gmail.com>
2024-01-04 00:07:26 +0000

    Jan 3, 2024, 7:07 PM

--- week0/tictactoe/tictactoe.py
+++ week0/tictactoe/tictactoe.py
@@ -175,7 +175,7 @@ def max_function(board):
         
         # set starting value to maximize against and empty best action
         v = -math.inf
-        best_action = 
+        best_action = set()
 
         # loop through the possible actions
         moves = actions(board)
