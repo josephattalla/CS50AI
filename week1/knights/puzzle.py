6e18448c2bbe861acbb1db3d18d56fc23c98fde8
Joseph Attalla <jojoatt10@gmail.com>
2024-01-05 21:51:14 +0000

    Jan 5, 2024, 4:51 PM

--- week1/knights/puzzle.py
+++ week1/knights/puzzle.py
@@ -11,7 +11,8 @@ CKnave = Symbol("C is a Knave")
 
 # Logic of game
 logic = And(Biconditional(AKnight, AKnave),
-            Biconditional(BKnight, BKnave))
+            Biconditional(BKnight, BKnave),
+            Biconditional(AKnight, AKnave))
 
 # Puzzle 0
 # A says "I am both a knight and a knave."
