ff02644c02c7ecd21f750ffef3ff2dda33cf3ee6
Joseph Attalla <jojoatt10@gmail.com>
2024-01-05 22:05:11 +0000

    Jan 5, 2024, 5:05 PM

--- week1/knights/puzzle.py
+++ week1/knights/puzzle.py
@@ -13,7 +13,7 @@ CKnave = Symbol("C is a Knave")
 # A says "I am both a knight and a knave."
 knowledge0 = And(Or(AKnave, AKnight),
                  Biconditional(AKnight, AKnave),
-                 )
+                 Implication(AKnave))
 
 # Puzzle 1
 # A says "We are both knaves."
