b798b0332b3eda668fa5ee724ab495794a6bc8e6
Joseph Attalla <jojoatt10@gmail.com>
2024-01-05 22:19:32 +0000

    Jan 5, 2024, 5:19 PM

--- week1/knights/puzzle.py
+++ week1/knights/puzzle.py
@@ -19,7 +19,8 @@ knowledge0 = And(Not(And(AKnave, AKnight)),
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
-knowledge1 = And(Implication(AKnave, BKnave))
+knowledge1 = And(Or(And(AKnave, BKnight), And(BKnave, AKnight)),
+                 )
 
 # Puzzle 2
 # A says "We are the same kind."
