013bcb017ed8d84e090927f729a8ee39bcda76fb
Joseph Attalla <jojoatt10@gmail.com>
2024-01-05 21:29:44 +0000

    Jan 5, 2024, 4:29 PM

--- week1/knights/logic.py
+++ week1/knights/logic.py
@@ -34,8 +34,7 @@ class Sentence():
                         return False
                     count -= 1
             return count == 0
-        if not len(s) or s.isalpha() or (
-            s[0] == "(" and s[-1] == ")" and balanced(s[1:-1])
+        if not len(s) or s.isalpha() or ( s[0] == "(" and s[-1] == ")" and balanced(s[1:-1])
         ):
             return s
         else:
