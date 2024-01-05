2ff543e72474c7b4a092dfe58e790f79ae6b8417
Joseph Attalla <jojoatt10@gmail.com>
2024-01-05 21:40:10 +0000

    Jan 5, 2024, 4:40 PM

--- week1/knights/puzzle.py
+++ week1/knights/puzzle.py
@@ -21,7 +21,7 @@ knowledge1 = And(Implication(AKnave, BKnave))
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
-knowledge2 = And()
+knowledge2 = And(Biconditional())
 
 # Puzzle 3
 # A says either "I am a knight." or "I am a knave.", but you don't know which.
