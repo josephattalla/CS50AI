07c94b3f79f8a6264c626b42a392126355b9fc04
Joseph Attalla <jojoatt10@gmail.com>
2024-01-05 22:28:27 +0000

    Jan 5, 2024, 5:28 PM

--- week1/knights/puzzle.py
+++ week1/knights/puzzle.py
@@ -33,8 +33,7 @@ knowledge2 = And(Not(And(AKnave, AKnight)),
                  Not(And(BKnave, BKnight)),
                  Or(BKnight, BKnave),
                  Implication(AKnave, Not(And(AKnave, BKnave))),
-                 Implication(BKnave, Not(And(BKnave, AKnight))),
-                 Implication)
+                 Implication(BKnave, Not(And(BKnave, AKnight))))
 
 # Puzzle 3
 # A says either "I am a knight." or "I am a knave.", but you don't know which.
