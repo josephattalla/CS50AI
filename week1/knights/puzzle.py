0323492f1c4ecb1ada151b33977d2db2eb121da4
Joseph Attalla <jojoatt10@gmail.com>
2024-01-05 21:57:12 +0000

    Jan 5, 2024, 4:57 PM

--- week1/knights/puzzle.py
+++ week1/knights/puzzle.py
@@ -19,7 +19,7 @@ logic = And(Biconditional(AKnight, AKnave),
 knowledge0 = And(
     Or(Implication(AKnight, And(AKnight, AKnave)), Implication(AKnave, And(AKnight, AKnave))),
        Biconditional(AKnight, AKnave),
-       Implication(AKnave))
+       Implication(AKnave, Not))
 
 # Puzzle 1
 # A says "We are both knaves."
